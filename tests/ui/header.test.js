396b2506b88e414e65be167bd5c46f96fafea9e3
ssllaavv <ssllaavv@gmail.com>
2024-04-11 13:18:02 +0300

    Update url in UI test for production

--- tests/ui/header.test.js
+++ tests/ui/header.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check header', async ({ page }) => {
-    await page.goto('http://localhost:8080/about');  // you can replace this URL with any page that includes the header
+    await page.goto('https://ssllaavv-boardgames-exam.onrender.com/about');  // you can replace this URL with any page that includes the header
     const homeLink = await page.$('a[href="/boardgames"]');
     const text = await homeLink.textContent();
     expect(text).toBe('View Boardgames');
